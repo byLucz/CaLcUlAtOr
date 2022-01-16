4cea23b368282ba4d236ff41c8ac6ccedc656771
KirillA1 <KirillA1@outlook.com>
2022-01-17 00:30:39 +0700

    dobavleno vtoroe chislo

--- cal4ik/cal4ik/Program.cs
+++ cal4ik/cal4ik/Program.cs
@@ -6,10 +6,16 @@ namespace cal4ik
     {
         static void Main(string[] args)
         {
-            int num1 = 0; 
+            int num1 = 0;
+            int num2 = 0;
 
             Console.WriteLine("Первое число");
             num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Второе число");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+
         }
     }
 }

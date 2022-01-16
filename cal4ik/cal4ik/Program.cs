eab9988606fc2acaecb8298c17d07dc5c5e968b3
Luc4sZ <72922298+byLucz@users.noreply.github.com>
2022-01-17 04:13:06 +0700

    YEAAS
    nice

--- cal4ik/cal4ik/Program.cs
+++ cal4ik/cal4ik/Program.cs
@@ -1,14 +1,49 @@
-﻿using System;
+using System;
 
 namespace cal4ik
 {
     class Program
     {
         static void Main(string[] args)
-        { 
-            while(true)
-            Console.WriteLine(DateTime.Now.ToLongTimeString());
+        {
+            int num1 = 0;
+            int num2 = 0;
 
+            Console.WriteLine("Первое число");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Второе число");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Менюшка");
+            Console.WriteLine("\t+ - плюс");
+            Console.WriteLine("\t- - минус");
+            Console.WriteLine("\t* - умножение");
+            Console.WriteLine("\t/ - деление");
+            Console.Write("Твой выбор?");
+
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    Console.WriteLine($"Ваш результат: {num1} + {num2} = " + (num1 + num2));
+                    break;
+
+                case "-":
+                    Console.WriteLine($"Ваш результат: {num1} - {num2} = " + (num1 - num2));
+                    break;
+
+                case "*":
+                    Console.WriteLine($"Ваш результат: {num1} * {num2} = " + (num1 * num2));
+                    break;
+
+                case "/":
+                    Console.WriteLine($"Ваш результат: {num1} / {num2} = " + (num1 / num2));
+                    break;
+
+            }
+
+            Console.Write("Press any key to close the Calculator console app...");
+            Console.ReadKey();
         }
     }
 }

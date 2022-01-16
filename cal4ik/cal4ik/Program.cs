642a5c837b3b1a91c773de803fe71d9fc5d8000b
KirillA1 <KirillA1@outlook.com>
2022-01-17 00:24:43 +0700

    start

---
+++ cal4ik/cal4ik/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cal4ik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
